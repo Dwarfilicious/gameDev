01a3fb32cb0ec346f65e1ec4211d6371c00acf96
Dwarfilicious <zoeteman1@gmail.com>
2023-12-30 05:39:31 +0100

    Update Map.cpp
    almost to a game logic testable level

--- maps/mapResources/Map.cpp
+++ maps/mapResources/Map.cpp
@@ -3,6 +3,7 @@
  * The map class, which contains all the tiles that make up the map. */
 
 #include <iostream>
+#include <GL/glut.h>
 #include "Map.h"
 
 Map::Map() {}
@@ -22,6 +23,58 @@ void Map::addBorderTile(const Tile& tile)
     borderTiles.push_back(tile);
 }
 
+bool outsideContinent(int x, int y, std::vector<int> data, int mapSizeX, int mapSizeY)
+{
+    return x < 0 || x >= mapSizeX || y < 0 || y >= mapSizeY || data[y * mapSizeY + x] == 2;
+}
+
+/* Helper function: using flood fill algorithm to add tiles to correct territories and territories
+ * to the correct continent. */
+void determineContinent(int x, int y, std::vector<int>& data,
+                        int mapSizeX, int mapSizeY, std::vector<std::vector<bool>>& visited,
+                        Continent& continent, Territory& territory)
+{
+    if (outsideContinent(x, y, data, mapSizeX, mapSizeY) || visited[y][x] == true)
+    {
+        return;
+    }
+
+    if (data[y * mapSizeX + x] == 3)
+    {
+        return;
+    }
+
+    if (territory.getTiles().empty())
+    {
+        continent.addTerritory(territory);
+    }
+
+    if (data[y * mapSizeX + x] == 1)
+    {
+        territory.addTile(Tile(x, y, 1));
+    }
+
+    visited[y][x] = true;
+
+    std::vector<std::pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    for (const std::pair<int, int>& direction : directions)
+    {
+        int dx = direction.first;
+        int dy = direction.second;
+        if (!outsideContinent(x + dx, y + dy, data, mapSizeX, mapSizeY) && data[y * mapSizeX + (x-1)] == 3)
+        {
+            Territory newTerritory("territory");
+            determineContinent(x + 2*dx, y + 2*dy, data, mapSizeX, mapSizeY, visited, continent, newTerritory);
+        }
+        else
+        {
+            determineContinent(x + dx, y + dy, data, mapSizeX, mapSizeY, visited, continent, territory);
+        }
+    }
+
+    return;
+}
+
 void Map::importMap(const std::string& fileName)
 {
     std::ifstream ifs(fileName);
@@ -31,6 +84,44 @@ void Map::importMap(const std::string& fileName)
 
     mapSizeX = jsonFile["layers"][0]["width"];
     mapSizeY = jsonFile["layers"][0]["height"];
+    std::vector<int> data = jsonFile["layers"][0]["data"].get<std::vector<int>>();
+
+    std::vector<std::vector<bool>> visited(mapSizeY, std::vector<bool>(mapSizeX, false));
+
+    for (int y = 0; y < mapSizeY; y++)
+    {
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            if (visited[y][x])
+            {
+                continue;
+            }
+
+            int tileType = data[y * mapSizeX + x];
+            Tile tile(x, y, tileType);
+            Continent newContinent("continent", 50);
+            Territory newTerritory("territory");
+
+            switch (tileType)
+            {
+            case 1:
+                addContinent(newContinent);
+                determineContinent(x, y, data, mapSizeX, mapSizeY,
+                                   visited, continents.back(), newTerritory);
+                break;
+            case 2:
+                addOceanTile(tile);
+                break;
+            case 3:
+                addBorderTile(tile);
+                break;
+            default:
+                std::cout << "Error: unknown tile type" << std::endl;
+                break;
+            }
+        }
+    }
+    std::cout << continents.size() << std::endl;
 }
 
 void Map::draw() const
@@ -51,8 +142,30 @@ void Map::draw() const
     }
 }
 
-int main()
+Map map;
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    map.draw();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
 {
     Map map;
     map.importMap("../Tiled/testMap.json");
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("Game");
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
 }
