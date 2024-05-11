01402a9bf0e1edd10fd3e1bd1e13f340c256137e
Dwarfilicious <zoeteman1@gmail.com>
2024-05-11 06:49:20 +0200

    switch to int ID's to accomodate == and !=

--- src/gameResources/mapResources/Territory.cpp
+++ src/gameResources/mapResources/Territory.cpp
@@ -6,8 +6,13 @@
 
 #include "Territory.hpp"
 
-Territory::Territory(const std::string& name)
-    : name(name), owner("Neutral"), color(0.5f, 0.5f, 0.5f) {}
+Territory::Territory(const int territoryID)
+    : territoryID(territoryID), owner(0), color(0.5f, 0.5f, 0.5f) {}
+
+void Territory::setName(const std::string& name)
+{
+    this->name = name;
+}
 
 void Territory::addTile(const Tile& tile)
 {
@@ -20,7 +25,12 @@ void Territory::setOwner(const Player& player)
     color = player.getColor();
 }
 
-std::string Territory::getName() const
+const int Territory::getID() const
+{
+    return territoryID;
+}
+
+const std::string Territory::getName() const
 {
     return name;
 }
@@ -45,4 +55,14 @@ void Territory::draw(int mapSizeX, int mapSizeY) const
     for (const Tile& tile : tiles) {
         tile.draw(color, mapSizeX, mapSizeY);
     }
+}
+
+bool operator==(const Territory& lhs, const Territory& rhs)
+{
+    return lhs.getID() == rhs.getID();
+}
+
+bool operator!=(const Territory& lhs, const Territory& rhs)
+{
+    return !(lhs == rhs);
 }
--- src/gameResources/mapResources/Territory.hpp
+++ src/gameResources/mapResources/Territory.hpp
@@ -16,20 +16,26 @@
 
 class Territory {
 public:
-    Territory(const std::string& name);
+    Territory(const int territoryID);
+    void setName(const std::string& name);
     void addTile(const Tile& tile);
     void setOwner(const Player& player);
-    std::string getName() const;
+    const int getID() const;
+    const std::string getName() const;
     const Player& getOwner() const;
     const Color& getColor() const;
     const std::vector<Tile>& getTiles() const;
     void draw(int mapSizeX, int mapSizeY) const;
 
 private:
+    int territoryID;
     std::string name;
     Player owner;
     Color color;
     std::vector<Tile> tiles;
 };
+
+bool operator==(const Territory& lhs, const Territory& rhs);
+bool operator!=(const Territory& lhs, const Territory& rhs);
 
 #endif /* TERRITORY_H */
